b5d798f19b7a4d76c301b0dbc79b5d9050eb3a4d
root <root@dappvm0.qfaqdgeot5relekurfs2xzgwmc.ax.internal.cloudapp.net>
2021-02-17 11:04:23 +0000

    update install caliper

--- install-caliper.sh
+++ install-caliper.sh
@@ -1,7 +1,9 @@
-cd caliper-benchmark-ben
+cd ~
 sudo apt-get install make g++
 sudo npm install -g node-gyp
 
+git clone https://github.com/AXinx/caliper-bench.git
+cd ./caliper-bench/celiper-benchmarks-ben/
 npm install
 npm install -g --only=prod @hyperledger/caliper-cli@0.2.0 --unsafe-perm
 npx caliper bind --caliper-bind-sut fabric --caliper-bind-sdk 1.4.4 --caliper-bind-cwd ./
