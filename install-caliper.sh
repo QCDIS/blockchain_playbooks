85bffd093427267158f6c276eb7785b10b002562
root <root@ip-172-31-23-149.ec2.internal>
2021-02-18 16:47:07 +0000

    revise caliper install

--- install-caliper.sh
+++ install-caliper.sh
@@ -3,7 +3,7 @@ sudo apt-get install make g++
 sudo npm install -g node-gyp
 
 git clone https://github.com/AXinx/caliper-bench.git
-cd ./caliper-bench/celiper-benchmarks-ben/
+cd ./caliper-bench/caliper-benchmarks-ben/
 npm install
 npm install -g --only=prod @hyperledger/caliper-cli@0.2.0 --unsafe-perm
 npx caliper bind --caliper-bind-sut fabric --caliper-bind-sdk 1.4.4 --caliper-bind-cwd ./
