949dbba87ed82349578585eb9c043147fd0e2bd1
AXinx <xin-yue-2009@163.com>
2021-03-09 02:15:56 +0800

    change file path

--- install-caliper.sh
+++ install-caliper.sh
@@ -3,7 +3,7 @@ sudo apt-get install make g++
 sudo npm install -g node-gyp
 
 git clone https://github.com/AXinx/caliper-bench.git
-cd ./caliper-bench/caliper-benchmarks-ben/
+cd ./caliper-bench/azure-3nodes/caliper-benchmarks/
 npm install
 npm install -g --only=prod @hyperledger/caliper-cli@0.2.0 --unsafe-perm
 npx caliper bind --caliper-bind-sut fabric --caliper-bind-sdk 1.4.4 --caliper-bind-cwd ./
