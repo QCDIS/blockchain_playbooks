d762638774ab68e46b652a3f02b48141dafec356
AXinx <xin-yue-2009@163.com>
2021-03-09 02:30:26 +0800

    change file path

--- caliper-benchmark.sh
+++ caliper-benchmark.sh
@@ -1,21 +1,21 @@
 cd ~
-cd /root/caliper-bench/caliper-benchmarks-ben/networks/fabric/fabric-v1.4.1/swarm-3org1peer-raft/config
+cd /root/caliper-bench/azure-3nodes/caliper-benchmarks/networks/fabric/fabric-v1.4.1/swarm-3org1peer-raft/config
 ./generate.sh
 
 cd ~
 
-ssh Node0 "mkdir -p /root/caliper-bench/caliper-benchmarks-ben/networks/fabric/fabric-v1.4.1/swarm-3org1peer-raft/"
-scp -r /root/caliper-bench/caliper-benchmarks-ben/networks/fabric/fabric-v1.4.1/swarm-3org1peer-raft/config/ Node0:/root/caliper-bench/caliper-benchmarks-ben/networks/fabric/fabric-v1.4.1/swarm-3org1peer-raft/
+ssh Node0 "mkdir -p /root/caliper-bench/azure-3nodes/caliper-benchmarks/networks/fabric/fabric-v1.4.1/swarm-3org1peer-raft/"
+scp -r /root/caliper-bench/azure-3nodes/caliper-benchmarks/networks/fabric/fabric-v1.4.1/swarm-3org1peer-raft/config/ compute1:/root/caliper-bench/caliper-benchmarks-ben/networks/fabric/fabric-v1.4.1/swarm-3org1peer-raft/
 
-ssh Node1 "mkdir -p /root/caliper-bench/caliper-benchmarks-ben/networks/fabric/fabric-v1.4.1/swarm-3org1peer-raft/"
-scp -r /root/caliper-bench/caliper-benchmarks-ben/networks/fabric/fabric-v1.4.1/swarm-3org1peer-raft/config/ Node1:/root/caliper-bench/caliper-benchmarks-ben/networks/fabric/fabric-v1.4.1/swarm-3org1peer-raft/
+ssh Node1 "mkdir -p /root/caliper-bench/azure-3nodes/caliper-benchmarks/networks/fabric/fabric-v1.4.1/swarm-3org1peer-raft/"
+scp -r /root/caliper-bench/azure-3nodes/caliper-benchmarks/networks/fabric/fabric-v1.4.1/swarm-3org1peer-raft/config/ compute2:/root/caliper-bench/caliper-benchmarks-ben/networks/fabric/fabric-v1.4.1/swarm-3org1peer-raft/
 
-ssh Node2 "mkdir -p /root/caliper-bench/caliper-benchmarks-ben/networks/fabric/fabric-v1.4.1/swarm-3org1peer-raft/"
-scp -r /root/caliper-bench/caliper-benchmarks-ben/networks/fabric/fabric-v1.4.1/swarm-3org1peer-raft/config/ Node2:/root/caliper-bench/caliper-benchmarks-ben/networks/fabric/fabric-v1.4.1/swarm-3org1peer-raft/
+ssh Node2 "mkdir -p /root/caliper-bench/azure-3nodes/caliper-benchmarks/networks/fabric/fabric-v1.4.1/swarm-3org1peer-raft/"
+scp -r /root/caliper-bench/azure-3nodes/caliper-benchmarks/networks/fabric/fabric-v1.4.1/swarm-3org1peer-raft/config/ compute3:/root/caliper-bench/caliper-benchmarks-ben/networks/fabric/fabric-v1.4.1/swarm-3org1peer-raft/
 
-cd /root/caliper-bench/monitor
+cd /root/caliper-bench/azure-3nodes/monitor
 ./start.sh
 
-cd ../caliper-benchmarks-ben/
+cd ../caliper-benchmarks/
 npx caliper benchmark run --caliper-benchconfig benchmarks/samples/fabric/marbles/config.yaml --caliper-networkconfig networks/fabric/fabric-v1.4.1/swarm-3org1peer-raft/fabric-ccp-go-tls.yaml --caliper-workspace .
 
