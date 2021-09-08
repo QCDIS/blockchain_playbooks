2ba6369ba3a2e4abf162289f13fceb28c8fb5878
skoulouzis <skoulouzis@users.noreply.github.com>
2021-09-08 13:35:41 +0200

    Update install-caliper.sh

--- install-caliper.sh
+++ install-caliper.sh
@@ -4,7 +4,7 @@ sudo npm install -g node-gyp
 
 git clone https://github.com/AXinx/caliper-bench.git
 cd ./caliper-bench/azure-3nodes/caliper-benchmarks/
-npm install
+npm install --save fs-extra 
 npm install -g --only=prod @hyperledger/caliper-cli@0.2.0 --unsafe-perm
 npx caliper bind --caliper-bind-sut fabric --caliper-bind-sdk 1.4.4 --caliper-bind-cwd ./
 
